debb3255a754fac85832429e6d19624e831f5d5d
unknown <142760953+scentier@users.noreply.github.com>
2023-09-17 01:59:15 +0700

    if else challange

--- Program.cs
+++ Program.cs
@@ -1,47 +1,27 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-int min = 1;
-int max = 7;
-
-int roll1 = dice.Next(min, max);
-int roll2 = dice.Next(min, max);
-int roll3 = dice.Next(min, max);
-
-int maxToWin = max * 2 + 1;
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+if (daysUntilExpiration == 0)
 {
-    if (roll1 == roll2 && roll2 == roll3)
-    {
-        Console.WriteLine("You rolled triples! +6 bonus to total");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("Your rolled doubles! +2 bonus to total!");
-        total += 2;
-    }
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
 
-
-if (total >= maxToWin + 1)
+if (discountPercentage > 0)
 {
-    Console.WriteLine("You win a new car!");
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
-else if (total >= 10)
-{
-    Console.WriteLine("You win a new laptop!");
-}
-else if (total == 7)
-{
-    Console.WriteLine("You win a trip for two");
-}
-else
-{
-    Console.WriteLine("You win a kitten");
-}
-
