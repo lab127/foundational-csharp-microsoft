00a0f087000e4895d951562405fe1907df556975
unknown <142760953+scentier@users.noreply.github.com>
2023-09-22 10:19:08 +0700

    reverse string & count 'o' char

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿// Example 1:
-Console
-.
-WriteLine
-(
-"Hello Example 1!"
-)
-;
+﻿/*
+reverse string by convert to array and use array reserve
+then convert back to string
+count total 'o' within the string by using foreach and add count
+*/
 
-// Example 2:
-string firstWord = "Hello"; string lastWord = "Example 2"; Console.WriteLine(firstWord + " " + lastWord + "!");
+string str = "The quick brown fox jumps over the lazy dog.";
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+int x = 0;
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+string new_message = new String(charMessage);
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
