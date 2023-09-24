346c999231be243246c769ab3286db2fa1a12be4
unknown <ronie2rush@gmail.com>
2023-09-24 14:59:26 +0700

    challage: update problem within code

--- VariableScopeAndLogic/Program.cs
+++ VariableScopeAndLogic/Program.cs
@@ -1,24 +1,23 @@
-﻿bool flag = true;
-/* 
-1. single line is valid but never use single-line form. example: if (flag) Console.WriteLine(flag);
-2. Using braces is always accepted, and required if any block of an if/else if/.../else statement
-3. Braces may be omitted only if the body of every block associated with an if/else if/.../else compound statement is placed on a single line.
-*/
-if (flag)
-    Console.WriteLine(flag);
+﻿int[] numbers = { 4, 8, 15, 16, 23, 42 };
 
+foreach (int number in numbers)
+{
+    int total;
 
-// one line valid code block
-string name = "steve";
-if (name == "bob") Console.WriteLine("Found Bob");
-else if (name == "steve") Console.WriteLine("Found Steve");
-else Console.WriteLine("Found Chuck");
+    total += number;
 
-// multi line code block without braces, more readable
-name = "bob";
-if (name == "bob")
-    Console.WriteLine("Found Bob");
-else if (name == "steve")
-    Console.WriteLine("Found Steve");
-else
-    Console.WriteLine("Found Chuck");
+    if (number == 42)
+    {
+        bool found = true;
+
+    }
+
+}
+
+if (found)
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
