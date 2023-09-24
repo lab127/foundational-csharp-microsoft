3956f1f0fc706d1860b64f56c7e8851a2017cdf6
unknown <ronie2rush@gmail.com>
2023-09-24 14:53:19 +0700

    rm braces within code block

--- VariableScopeAndLogic/Program.cs
+++ VariableScopeAndLogic/Program.cs
@@ -1,9 +1,24 @@
 ﻿bool flag = true;
-int value = 0; // must be asigned
+/* 
+1. single line is valid but never use single-line form. example: if (flag) Console.WriteLine(flag);
+2. Using braces is always accepted, and required if any block of an if/else if/.../else statement
+3. Braces may be omitted only if the body of every block associated with an if/else if/.../else compound statement is placed on a single line.
+*/
 if (flag)
-{
-    value = 10;
-    Console.WriteLine("Inside of code block: " + value);
-}
+    Console.WriteLine(flag);
 
-Console.WriteLine($"Outside of code block: {value}");
+
+// one line valid code block
+string name = "steve";
+if (name == "bob") Console.WriteLine("Found Bob");
+else if (name == "steve") Console.WriteLine("Found Steve");
+else Console.WriteLine("Found Chuck");
+
+// multi line code block without braces, more readable
+name = "bob";
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve")
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
