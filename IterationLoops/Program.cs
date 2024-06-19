c6bbd18e18377c567ab0caba03b6ab3dfe48c9af
unknown <12640394+lab127@users.noreply.github.com>
2024-06-19 12:09:29 +0700

    Loops

---
+++ IterationLoops/Program.cs
@@ -0,0 +1,47 @@
+﻿for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine($"\"i += 3\" to skip two values after each iteration. i = {i}");
+}
+
+Console.WriteLine("-------------");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Increment by 1. i = {i}");
+    if (i == 3) break;
+}
+
+Console.WriteLine("-------------");
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine($"Decrement by 1. i = {i}");
+    if (i == 2) break;
+}
+
+Console.WriteLine("-------------");
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+// foreach (var name in names)
+// {
+//     if (name == "David") name = "Sammy";
+//     Console.WriteLine(name);
+// }
+
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
