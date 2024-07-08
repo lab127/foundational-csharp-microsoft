53dcb8bbbacfe264cd5993c6ede224574cc1e123
unknown <12640394+lab127@users.noreply.github.com>
2024-07-08 17:37:03 +0700

    while loop

--- DoWhileLoop/Program.cs
+++ DoWhileLoop/Program.cs
@@ -2,8 +2,22 @@
 
 int current = 0;
 
+// The do-while statement executes a statement or a block of statements while a specified Boolean expression evaluates to true. 
+/**
 do
 {
     current = random.Next(1, 11);
     Console.WriteLine(current);
 } while (current != 7);
+*/
+
+current = random.Next(1, 11);
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    // assign new random number to current variable
+    current = random.Next(1, 11);
+}
+
+Console.WriteLine($"Last number: {current}");
